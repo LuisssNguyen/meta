1897cb20f87f28c660e1f73ec1f61203c0eb4cb5
LuisssNguyen <LuisSarchi146@gmail.com>
2024-05-20 15:42:29 +0700

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Policy.module.scss';
-import images from '~/assets/images';
+import images from '../../assets/images';
 
 const cx = classNames.bind(styles);
 
